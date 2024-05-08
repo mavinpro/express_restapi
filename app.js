494aa7957fbd88e46f5854d2ffa07f3a65d53a48
mavinpro <marchelinokevin12@outlook.com>
2024-05-08 08:52:20 +0700

    adding try catch error handling in app.js

--- app.js
+++ app.js
@@ -27,59 +27,104 @@ var kelas = [
   { id: 3, nama_kelas: "Fullstack" },
 ];
 app.get("/api/kelas", function (req, res) {
-  res.json({ data: kelas });
-  res.send({ data: kelas });
+  try {
+    res.json({ data: kelas });
+    res.send({ data: kelas });
+    res.send(err);
+  } catch (err) {
+    res.status(500).json({
+      status: false,
+      name: err.name,
+      message: err.message,
+    });
+  }
 });
 
 app.get("/api/kelas/:id", function (req, res) {
-  const kls = kelas.find((k) => k.id === parseInt(req.params.id));
-  if (!kls) res.status(404).send("Kelas tidak ditemukan"); // tampilkan status 404
-  res.json({ data: kls });
-  res.send({ data: kls });
+  try {
+    const kls = kelas.find((k) => k.id === parseInt(req.params.id));
+    if (!kls) res.status(404).send("Kelas tidak ditemukan"); // tampilkan status 404
+    res.json({ data: kls });
+    res.send({ data: kls });
+    res.send(err);
+  } catch (err) {
+    res.status(500).json({
+      status: false,
+      name: err.name,
+      message: err.message,
+    });
+  }
 });
 
 // Menambahkan data
 app.post("/api/kelas", function (req, res) {
-  //Kondisi apabila nama kelas kosong
-  if (!req.body.nama_kelas) {
-    // Menampilkan pesan error ketika field nama kelas kosong
-    res.status(400).send("Nama kelas harus diisi");
-    return;
-  }
+  try {
+    //Kondisi apabila nama kelas kosong
+    if (!req.body.nama_kelas) {
+      // Menampilkan pesan error ketika field nama kelas kosong
+      res.status(400).send("Nama kelas harus diisi");
+      return;
+    }
 
-  const kls = {
-    id: kelas.length + 1,
-    nama_kelas: req.body.nama_kelas,
-  };
-  kelas.push(kls);
-  res.send(kls);
+    const kls = {
+      id: kelas.length + 1,
+      nama_kelas: req.body.nama_kelas,
+    };
+    kelas.push(kls);
+    res.send(kls);
+    res.send(err);
+  } catch (err) {
+    res.status(500).json({
+      status: false,
+      name: err.name,
+      message: err.message,
+    });
+  }
 });
 
 // Mengupdate data
 app.put("/api/kelas/:id", function (req, res) {
-  //Cek id kelas
-  const klas = kelas.find((k) => k.id === parseInt(req.params.id));
-  if (!klas) res.status(404).send("Kelas tidak ditemukan"); // tampilkan status 404
+  try {
+    //Cek id kelas
+    const klas = kelas.find((k) => k.id === parseInt(req.params.id));
+    if (!klas) res.status(404).send("Kelas tidak ditemukan"); // tampilkan status 404
 
-  if (!req.body.nama_kelas) {
-    // Menampilkan pesan error ketika field nama kelas kosong
-    res.status(400).send("Nama kelas harus diisi");
-    return;
+    if (!req.body.nama_kelas) {
+      // Menampilkan pesan error ketika field nama kelas kosong
+      res.status(400).send("Nama kelas harus diisi");
+      return;
+    }
+
+    klas.nama_kelas = req.body.nama_kelas;
+    res.send({ pesan: "Data berhasil diupdate.", data: klas });
+    res.send(err);
+  } catch (err) {
+    res.status(500).json({
+      status: false,
+      name: err.name,
+      message: err.message,
+    });
   }
-
-  klas.nama_kelas = req.body.nama_kelas;
-  res.send({ pesan: "Data berhasil diupdate.", data: klas });
 });
 
 // Menghapus data
 app.delete("/api/kelas/:id", function (req, res) {
-  //Cek id kelas
-  const klas = kelas.find((k) => k.id === parseInt(req.params.id));
-  if (!klas) res.status(404).send("Kelas tidak ditemukan"); // tampilkan status 404
+  try {
+    //Cek id kelas
+    const klas = kelas.find((k) => k.id === parseInt(req.params.id));
+    if (!klas) res.status(404).send("Kelas tidak ditemukan"); // tampilkan status 404
 
-  const index = kelas.indexOf(klas);
-  kelas.splice(index, 1);
-  res.send({ pesan: "Data berhasil dihapus.", data: klas });
+    const index = kelas.indexOf(klas);
+    kelas.splice(index, 1);
+    res.send({ pesan: "Data berhasil dihapus.", data: klas });
+    res.send(err);
+  } catch (err) {
+    res.status(500).json({
+      status: false,
+      name: err.name,
+      message: err.message,
+    });
+  }
 });
 
 //Letakkan kode script AUTH setelah proses crud
